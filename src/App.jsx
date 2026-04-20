5f5c795b1cda71fd9bfc19bec823ee631c6cb217
Iftikharul-Rafi <iftikharulrafi@gmail.com>
2026-04-20 16:46:40 +0600

    Fix routing and add docs folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';   // ← BrowserRouter সরিয়েছি
 import { TimelineProvider } from './context/TimelineContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -12,22 +12,20 @@ import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <TimelineProvider>
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/friend/:id" element={<FriendDetail />} />
-              <Route path="/timeline" element={<Timeline />} />
-              <Route path="/stats" element={<Stats />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-          <Toaster position="top-right" />
-        </div>
-      </BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/friend/:id" element={<FriendDetail />} />
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
     </TimelineProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';   // ← ইম্পোর্ট যোগ করো
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/PH-Assignment-07">   {/* ← basename সেট করো */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
